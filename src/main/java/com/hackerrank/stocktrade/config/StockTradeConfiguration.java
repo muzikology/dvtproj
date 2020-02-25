cf63ed2f381ae3fa982087061ac6d6ac1cd33ca5
Muzi <muzikhubeka@gmail.com>
2020-02-25 20:00:06 +0200

    stocktrade spring

---
+++ src/main/java/com/hackerrank/stocktrade/config/StockTradeConfiguration.java
@@ -0,0 +1,33 @@
+package com.hackerrank.stocktrade.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
+import java.util.Properties;
+
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class StockTradeConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+
+    @Bean
+    public Hibernate5Module hibernate5Module() {
+        return new Hibernate5Module();
+    }
+
+
+}
---
+++ src/main/java/com/hackerrank/stocktrade/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.hackerrank.stocktrade.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@ComponentScan
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket StockTradeApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("StockTrade REST API")
+                .description("Abbot API Documentation.").termsOfServiceUrl("")
+                .license("STOCKTRADE")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .version("0.0.1")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("authkey", "Authorization", "header");
+    }
+}
